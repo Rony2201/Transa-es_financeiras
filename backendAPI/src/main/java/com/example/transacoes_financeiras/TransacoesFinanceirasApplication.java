81904875fef3b87d65e9a5307a4a963ba460f87c
Rony <roniely_pinheiro@hotmail.com>
2025-05-23 20:13:11 -0300

    Criando repositório do projeto Transações Financeiras, utilizando Java spring boot com Angular

---
+++ backendAPI/src/main/java/com/example/transacoes_financeiras/TransacoesFinanceirasApplication.java
@@ -0,0 +1,13 @@
+package com.example.transacoes_financeiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacoesFinanceirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacoesFinanceirasApplication.class, args);
+	}
+
+}
---
+++ backendAPI/src/test/java/com/example/transacoes_financeiras/TransacoesFinanceirasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transacoes_financeiras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesFinanceirasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
